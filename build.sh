61c689229ff40d930667031efd5518da4b0fe547
yxiaochun <545408760@qq.com>
2017-11-02 14:34:41 +0800

    ..

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd Kernel
 make
-cp arch/arm64/boot/Image /home/gtx1049/mkbootimg_tools-master/bootnew_A5108/kernel
-cd /home/gtx1049/mkbootimg_tools-master
-./mkboot bootnew_A5108 bootnew1.img
+cp arch/arm64/boot/Image ../mkbootimg_tools-master/A5108/kernel
+cd ../mkbootimg_tools-master
+#./mkboot A5108 bootnew1.img
 #cp arch/arm64/boot/Image /home/gtx1049/mkbootimg_tools-master/A5108/kernel
 #cd /home/gtx1049/mkbootimg_tools-master
-#./mkboot A5108 boot.img
-#adb push boot.img /sdcard/
-#adb shell "su -c dd if=/mnt/shell/emulated/0/boot.img of=/dev/block/mmcblk0p10"
-#adb reboot
+./mkboot A5108 boot.img
+adb push boot.img /sdcard/
+adb shell "su -c dd if=/mnt/shell/emulated/0/boot.img of=/dev/block/mmcblk0p10"
+adb reboot
